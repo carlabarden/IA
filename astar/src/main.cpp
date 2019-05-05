acd28a3fced6065164aa5b434649dc258c7aa6c2
Carla Barden <carla.barden@ecomp.ufsm.br>
2019-05-05 19:41:47 -0300

    ***

--- astar/src/main.cpp
+++ astar/src/main.cpp
@@ -3,7 +3,7 @@
 //para armazenar resultado final de distancia e tempo de cada iteração
 typedef struct {
     float distancia;
-    double tempo;
+    long double tempo;
 }Data;
 
 int main(int argc, char* argv[]) {
@@ -45,17 +45,18 @@ int main(int argc, char* argv[]) {
         if(caminho){
             Data aux;
             aux.distancia = cubo.dist;
-            aux.tempo = t_final.tv_sec  - t_inicio.tv_sec;
+            aux.tempo = t_final.tv_nsec  - t_inicio.tv_nsec;
             dados.push_back(aux);
         }
             
         fim++;
+        //printf("%d\n",fim);
     }while(fim < num_rod);
 
     //imprimindo na tela para redirecionar para arquivo
-    printf(" Tempo ; Dist G ; \n");
+    printf(" G (u) ; Tempo (ns) ; \n");
     for (unsigned i = 0; i < dados.size(); i++){
-        printf(" %2.lf;    %2.f;\n", dados.at(i).tempo, dados.at(i).distancia);
+        printf("%.0f;    %2.Lf;\n", dados.at(i).distancia, dados.at(i).tempo);
     }
 
     //liberando memória
